d5a78653f9d5a22c00836cc5d2d3956b34061ffc
ArceLopera <arcelopera.carlos@gmail.com>
2021-06-09 09:53:05 -0500

    detail with icon

--- KapautuChroma/AboutBoxChroma.Designer.cs
+++ KapautuChroma/AboutBoxChroma.Designer.cs
@@ -150,7 +150,6 @@
             this.ClientSize = new System.Drawing.Size(652, 494);
             this.Controls.Add(this.tableLayoutPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
